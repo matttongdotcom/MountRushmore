7656ed8eedc58eb7ca002dedceef4f6fb5db8b4e
matthewtong <tong.ma@northeastern.edu>
2025-06-04 15:52:37 -0400

    Initial Commit

---
+++ MountRushmore/MountRushmoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountRushmoreApp.swift
+//  MountRushmore
+//
+//  Created by Matthew Tong on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MountRushmoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MountRushmoreTests/MountRushmoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  MountRushmoreTests.swift
+//  MountRushmoreTests
+//
+//  Created by Matthew Tong on 6/4/25.
+//
+
+import Testing
+@testable import MountRushmore
+
+struct MountRushmoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
