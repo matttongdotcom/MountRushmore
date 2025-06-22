2932da2688837348d61deb434fe2ca806361e82a
matthewtong <matttongdotcom@gmail.com>
2025-06-22 19:34:53 -0400

    api request utility

--- MountRushmore/DraftDetails/DraftDetailsRepository.swift
+++ MountRushmore/DraftDetails/DraftDetailsRepository.swift
@@ -1,30 +1,16 @@
 import Foundation
 
 struct DraftDetailsRepository {
+    private let api: APIRequest
+
+    init(api: APIRequest = APIRequest()) {
+        self.api = api
+    }
+
     func getDraftDetails(draftId: String) async throws -> Draft {
-        guard let url = URL(string: "https://us-central1-mount-rushmore-cde9b.cloudfunctions.net/getDraft/\(draftId)") else {
-            throw URLError(.badURL)
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        let (data, response) = try await URLSession.shared.data(for: request)
-
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw URLError(.badServerResponse)
-        }
-
-        print("HTTP Status Code: \(httpResponse.statusCode)")
-        if let dataString = String(data: data, encoding: .utf8) {
-            print("Response data: \(dataString)")
-        }
-
-        guard (200...299).contains(httpResponse.statusCode) else {
-            throw URLError(.badServerResponse)
-        }
-
-        return try JSONDecoder().decode(Draft.self, from: data)
+        return try await api.makeRequest(
+            httpMethod: .GET,
+            path: "https://us-central1-mount-rushmore-cde9b.cloudfunctions.net/getDraft/\(draftId)"
+        )
     }
 } 
